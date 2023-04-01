07f2753d4395e187137773345ef90003dc933251
CaribouJohn <john@cariboujohn.net>
2023-04-01 15:43:07 +0100

    Add linux open

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,8 @@ int main()
     std::cout << "pisamreader" << std::endl;
 
     try {
-        auto genfile = proisam::ProisamFile::open("C:\\development\\experiment\\genfile.pro", 0);
+        //auto genfile = proisam::ProisamFile::open("C:\\development\\experiment\\genfile.pro", 0);
+        auto genfile = proisam::ProisamFile::open("/home/john/experiment/genfile.pro", 0);
     }
     catch (const proisam::ProisamException& e) {
         std::cout << e.what() << std::endl;
